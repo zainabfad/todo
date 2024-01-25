2128cdeb91365fe9b8eba622650fb915ec36010d
Zainab Omolola Fadeyi <70825458+Zainabfadeyi@users.noreply.github.com>
2024-01-25 09:20:18 +0100

    fixes

--- src/test/java/project/Todolist/TodoListApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package project.Todolist;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TodoListApplicationTests {
-
-
-}
--- src/test/java/project/Todolist/api/controller/TaskControllerTest.java
+++
@@ -1,7 +0,0 @@
-package project.Todolist.api.controller;
-
-import junit.framework.TestCase;
-
-public class TaskControllerTest extends TestCase {
-
-}
